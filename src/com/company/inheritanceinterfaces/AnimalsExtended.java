6e4f55e6e60ed824f009e7cf836c43a927dae09e
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-09-15 19:08:07 +0200

    Solves Arv & gränssnitt - task 4

---
+++ src/com/company/inheritanceinterfaces/AnimalsExtended.java
@@ -0,0 +1,21 @@
+package com.company.inheritanceinterfaces;
+
+public class AnimalsExtended {
+
+}
+
+class Bird extends Animal {
+
+}
+
+class Reptile extends Animal {
+
+}
+
+class Mammal extends Animal {
+
+}
+
+class Dinosaur extends Animal {
+
+}
