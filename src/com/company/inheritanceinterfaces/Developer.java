13b59d485a2d53d0d46d8ca035983501e0487323
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-09-15 21:53:00 +0200

    Solves arv&gränssnitt - task 10

---
+++ src/com/company/inheritanceinterfaces/Developer.java
@@ -0,0 +1,9 @@
+package com.company.inheritanceinterfaces;
+
+public interface Developer {
+    int programmingLanguages();
+    int yearsOfExperience();
+    boolean universityDegree();
+    boolean versionControl();
+}
+
---
+++ src/com/company/inheritanceinterfaces/JuniorDeveloper.java
@@ -0,0 +1,47 @@
+package com.company.inheritanceinterfaces;
+
+import java.util.ArrayList;
+
+public class JuniorDeveloper implements Developer{
+    int programmingLanguages;
+    boolean universityDegree;
+    int yearsOfExperience;
+    boolean versionControl;
+    boolean java;
+
+    public JuniorDeveloper() {
+
+    }
+
+    public JuniorDeveloper(int languages, boolean degree, int experience, boolean git) {
+        this.programmingLanguages = languages;
+        this.universityDegree = degree;
+        this.yearsOfExperience = experience;
+        this.versionControl = git;
+    }
+
+    @Override
+    public int programmingLanguages() {
+        return programmingLanguages;
+    }
+
+    @Override
+    public boolean universityDegree() {
+        return universityDegree;
+    }
+
+    @Override
+    public int yearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    @Override
+    public boolean versionControl() {
+        return versionControl;
+    }
+
+    public void java(boolean java){
+        this.java = java;
+    }
+    
+}
